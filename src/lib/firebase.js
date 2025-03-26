72f8d6f9c20ea54bfeb08ae7a833245ee8eaccbb
ajaykumar-21 <ajaykumarpk789@gmail.com>
2025-03-26 17:30:31 +0530

    create firebase file to keep firebase config

---
+++ src/lib/firebase.js
@@ -0,0 +1,27 @@
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+// Firebase configuration details
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+};
+
+// Initializes the Firebase application with the provided configuration
+const app = initializeApp(firebaseConfig);
+console.log(app);
+// Initialize Firebase authentication and database
+
+const auth = getAuth(app);
+const db = getFirestore(app);
+const googlProvider = new GoogleAuthProvider();
